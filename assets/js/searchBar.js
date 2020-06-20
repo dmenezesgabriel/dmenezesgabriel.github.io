fb0ef34d0d598b29b3dc28207b2357032e3dabe1
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2020-06-20 17:28:19 -0300

    Add search bar

---
+++ assets/js/searchBar.js
@@ -0,0 +1,44 @@
+$(function () {
+  $('#search-result').hide();
+  var posts = [];
+  $.get('/api/jekyll/posts.json', function (data) {
+    posts = data;
+  });
+  $('#search').on('keyup', function () {
+    var keyword = this.value.toLowerCase();
+    var searchResult = [];
+
+    if (keyword.length > 0) {
+      console.log(keyword.length);
+      $('#search-result').show();
+    } else {
+      $('#search-result').hide();
+    }
+    $('.result-item').remove();
+
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+      if (
+        post.title.toLowerCase().indexOf(keyword) >= 0 ||
+        post.description.toLowerCase().indexOf(keyword) >= 0
+      ) {
+        searchResult.push(post);
+      }
+    }
+    if (searchResult.length === 0) {
+      $('#search-result').append(
+        '<div class="result-item"><div class="description">There is no search result.</div></div>'
+      );
+    } else {
+      for (var i = 0; i < searchResult.length; i++) {
+        $('#search-result').append(
+          '<a class="result-item" href="' +
+            searchResult[i].url +
+            '"><div class="title">【' + searchResult[i].category + '】' +
+            searchResult[i].title +
+            '</div></a>'
+        );
+      }
+    }
+  });
+});
