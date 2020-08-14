b4ed79d74d706c1b1d754b2fade1d7eaf689ab79
dmenezesgabriel <dmenezes.gabriel@gmail.com>
2020-08-14 20:32:11 +0000

    "fe9c5d2 - "6d92c64 - "f11d88c - "da76828 - "d0b231d - "d8faa45 - Merge pull request #19 from dmenezesgabriel/update/index-layout-cards-size
    Update cards to same size""""""

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello World!!  ","categories": [],
+        "tags": [],
+        "url": "https://dmenezesgabriel.github.io/hello-world/",
+        "teaser": "https://dmenezesgabriel.github.io/assets/images/fallback-teaser.jpg"
+      }]
