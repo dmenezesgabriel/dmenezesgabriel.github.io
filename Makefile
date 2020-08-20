5880737ee71dbf148f51ee384a56c609f0afd732
dmenezesgabriel <dmenezes.gabriel@gmail.com>
2020-08-20 00:46:00 +0000

    "bbff569 - "b4ed79d - "fe9c5d2 - "6d92c64 - "f11d88c - "da76828 - "d0b231d - "d8faa45 - Merge pull request #19 from dmenezesgabriel/update/index-layout-cards-size
    Update cards to same size""""""""

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ build-dev:
 
 run-dev:
 	bundle exec jekyll serve --watch
+
+run-dev-draft:
+	bundle exec jekyll serve --watch --draft
--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Hello World",
-        "excerpt":"Hello World!!  ","categories": [],
-        "tags": [],
-        "url": "https://dmenezesgabriel.github.io/hello-world/",
+        "title": "Hello World!",
+        "excerpt":"Tradicionalmente, programas Olá, Mundo! são utilizados para demonstração no desenvolvimento de programas, assim como para assegurar que tanto uma linguagem como o sistema operacional estão funcionando corretamente.   Geralmente estes programas são os primeiros desenvolvidos por novos programadores, porque mesmo com pouca ou nenhuma experiência são fáceis para se executar, e por sua simplicidade são utilizados para garantir que os componentes de uma linguagem (seu compilador, ambiente de desenvolvimento e tempo de execução) foram instalados corretamente.   Os hackers também utilizam o Olá, Mundo! como prova de conceito de que o código arbitrário pode ser executado por meio de um exploit onde os designers do sistema não pretendiam que o código fosse executado.   Outro uso popular do Olá, Mundo! é como base de comparação. Desenvolvedores podem comparar o tamanho do executável que a linguagem gera e quanta infraestrutura de suporte deve existir por trás do programa para que ele seja executado.   def hello():     print('Hello World!') ","categories": ["python"],
+        "tags": ["python"],
+        "url": "https://dmenezesgabriel.github.io/python/hello-world/",
         "teaser": "https://dmenezesgabriel.github.io/assets/images/fallback-teaser.jpg"
       }]
