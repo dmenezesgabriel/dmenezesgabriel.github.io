53fb26496c3933521833fd044fe87783ecb1cf07
dmenezesgabriel <dmenezes.gabriel@gmail.com>
2022-06-18 19:14:08 +0000

    "4ced57f - "12ea87d - "aadd9d2 - "58b03ea - "2e211ca - "141ea83 - "e5a728f - "6b549f2 - "38f138a - "dbf2427 - "b1b3c85 - "ff43e55 - "a693f1f - "47d542d - "72813d4 - "fc28319 - "a237452 - "cecab16 - "0ceb8df - "cb2e898 - "18bab82 - "17936ae - "db2e007 - "a00fa55 - "13003b8 - "3ac6cb3 - "c0af45e - "cd7786d - "1ad6b3a - "1b03c7d - "08c90d5 - "2476700 - "581ff1c - "5c3b029 - "b63505f - "ff3cf87 - "2940b60 - "588527d - "34f0303 - "1d3d9de - "9ef9cc1 - "4a071bd - "b3b9ae6 - "e5362b2 - "413d6ba - "96e5e6d - "716a449 - "8600441 - "7e04e63 - "1dcb090 - "4aac9cb - "cf87287 - "b06db58 - "0252cc1 - "c743d9c - "1371cdd - "867dd6b - "71db7d5 - "9f62b29 - "57d6985 - "86e2261 - "328abbb - "f18eb74 - "3b79b7a - "4f16126 - "1b314bf - "ecaa775 - "f9f1bb8 - "85ea692 - "456b0a0 - "723bf02 - "b057561 - "704d56d - "ff6134c - "6560447 - "9ae72f3 - "914b7e4 - "2e64eb1 - "4e13db2 - "7f25c6f - "da564df - "11a6f70 - "3633bce - "6778c91 - "06e7b16 - "a396511 - "a869e46 - "e033362 - "43d1b2b - "6ce0ed9 - "c92ddf3 - "0548429 - "2cffea3 - "916844b - "b4b16d4 - "4e891a7 - "ff1bffd - "0fe7a46 - "a61c74b - "6a5e3f5 - "c330e77 - "534e882 - "c349031 - "04e917e - "aaaa917 - "cb27d9f - "5d3e5b0 - "e264ea1 - "ce302ba - "784ac0b - "767a483 - "70c2354 - "e7fa2dc - "c121050 - "2782e10 - "765515c - "86b5db8 - "1071c76 - "8c904c7 - "30fc7c0 - "b71c5ad - "18cda3b - "f41a9c0 - "d17d206 - "6305424 - "2d27117 - "127f704 - "71b40ce - "c3dd727 - "6c7c853 - "b99875a - "d0fa713 - "645e421 - "cdf09ab - "98d65dc - "3a8a986 - "2b0917f - "6d74a62 - "1842b3b - "d3bff25 - "e5163a8 - "8c6229f - "7d3d460 - "fc65d79 - "b89670a - "732a05f - "fb33f09 - "8a14391 - "c5148c3 - "27509c2 - "d11ea17 - "0f6797e - "1970dbb - "8a62ed0 - "4be9be4 - "c474368 - "95bcd10 - "9e18607 - "434ec4b - "30fca24 - "0a070e9 - "1d52836 - "7cd21f0 - "873d0c3 - "429aef2 - "0cfd43d - "55e8987 - "f0f5528 - "3a18233 - "741e26a - "38dd00c - "9734278 - "717946a - "bd75acc - "edd36fa - "2b8fe6c - "b6a35d9 - "5880737 - "bbff569 - "b4ed79d - "fe9c5d2 - "6d92c64 - "f11d88c - "da76828 - "d0b231d - "d8faa45 - Merge pull request #19 from dmenezesgabriel/update/index-layout-cards-size
    Update cards to same size""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,49 @@
-document.querySelectorAll("pre.highlight").forEach(function (pre) {
-  var button = document.createElement("button");
-  var copyText = "Copy";
-  button.className = "copy";
-  button.type = "button";
-  button.ariaLabel = "Copy code to clipboard";
-  button.innerText = copyText;
-  button.addEventListener("click", function () {
-    var code = pre.querySelector("code").innerText.trim();
-    navigator.clipboard.writeText(code);
-    button.innerText = "Copied";
-    setTimeout(function () {
-      button.innerText = copyText;
-    }, 4000);
+function generateCopyCodeButton() {
+  const codeSnippet = document.querySelectorAll("pre.highlight");
+  codeSnippet.forEach(function (pre) {
+    //  Create button element
+    let button = document.createElement("button");
+    let copyText = "Copy";
+    button.className = "copy";
+    button.type = "button";
+    button.ariaLabel = "Copy code to clipboard";
+    button.innerText = copyText;
+    // Add button event
+    button.addEventListener("click", function () {
+      let code = pre.querySelector("code").innerText.trim();
+      navigator.clipboard.writeText(code);
+      button.innerText = "Copied";
+      setTimeout(function () {
+        button.innerText = copyText;
+      }, 4000);
+    });
+    // Add button to snippet
+    pre.appendChild(button);
   });
-  pre.appendChild(button);
-});
+}
+
+function resizeIframe(object) {
+  object.style.height = object.contentWindow.document.body.scrollHeight + 50 + "px";
+  console.log(object.style.height);
+}
+
+function generateCodeOutput() {
+  const codeSnippet = document.querySelectorAll("div.highlight");
+  codeSnippet.forEach(function (codeSnippet) {
+    let pre = codeSnippet.querySelector("pre.highlight");
+    let languageDiv = codeSnippet.parentElement;
+    if (languageDiv.classList.contains("language-html")) {
+      let iframe = document.createElement("iframe");
+      let code = pre.querySelector("code").innerText.trim();
+      iframe.srcdoc = code;
+      iframe.className = "code-output";
+      languageDiv.parentElement.insertBefore(iframe, languageDiv.nextElementSibling);
+      iframe.onload = () => {
+        resizeIframe(iframe);
+      };
+    }
+  });
+}
+
+generateCopyCodeButton();
+generateCodeOutput();
